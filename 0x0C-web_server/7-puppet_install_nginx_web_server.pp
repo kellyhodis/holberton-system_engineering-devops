b712d4c282651bdc83a38b9f986623b3474f6ae7
kellyhodis <668@holbertonschool.com>
2019-08-08 11:13:03 -0700

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,10 +1,10 @@
 exec {"/usr/bin/env sudo apt-get -y update": }
 exec {"/usr/bin/env sudo apt-get -y install nginx": }
 exec {"/usr/bin/env sudo service nginx start": }
-exec {"/usr/bin/env echo 'Holberton School' > /var/www/html/index.nginx-debian.html": }
-exec {"/usr/bin/env new_string="}\n\n\trewrite ^\/redirect_me intranet.hbtn.io permanent;\n"": }
-exec {"/usr/bin/env sudo sed -i "0,/}/s//$new_string/" /etc/nginx/sites-enabled/default": }
-exec {"/usr/bin/env echo "Ceci n'est pas une page" > /var/www/html/404.html": }
-exec {"/usr/bin/env new_error_string="}\n\n\terror_page 404 \/404.html;\n"": }
-exec {"/usr/bin/env sudo sed -i "0,/}/s//$new_error_string/" /etc/nginx/sites-enabled/default: }
+exec {'/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html': }
+exec {'/usr/bin/env new_string="}\n\n\trewrite ^\/redirect_me intranet.hbtn.io permanent;\n"': }
+exec {'/usr/bin/env sudo sed -i "0,/}/s//$new_string/" /etc/nginx/sites-enabled/default': }
+exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/404.html': }
+exec {'/usr/bin/env new_error_string="}\n\n\terror_page 404 \/404.html;\n"': }
+exec {'/usr/bin/env sudo sed -i "0,/}/s//$new_error_string/" /etc/nginx/sites-enabled/default': }
 exec {"/usr/bin/env sudo service nginx restart": }
