2ec25f7bbb0a70827ac56b236dbb274ac26a1aed
Kelly Hodis <668@holbertonschool.com>
2019-08-09 20:06:44 -0400

    Create 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This is a script that lists all states with a name starting with N
+from the database hbtn_0e_0_usa."""
+
+import MySQLdb
+import sys
+
+
+def list_all_n(mysql_username="", mysql_password="", database_name=""):
+    ''' This is a script that lists all states with a name starting with N.'''
+    conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                           passwd=mysql_password, db=database_name,
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    list_all_n(username, password, database)
